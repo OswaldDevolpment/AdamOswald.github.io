759680ef4c5d2b8d5a592125cf9393d7e478f930
Restyled.io <commits@restyled.io>
2023-05-11 14:46:06 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fetch = require('node-fetch')
-const readline = require('readline-promise').default.createInterface({
-  input : process.stdin,
-  output : process.stdout
-})
-let obj = require('./types.json')
-const fs = require('fs')
+const fetch = require("node-fetch");
+const readline = require("readline-promise").default.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let obj = require("./types.json");
+const fs = require("fs");
 
 console.log(`
       _     _                     _       
@@ -14,88 +14,101 @@ _ __ | |__ | |_  __  ______   _ __ _ _ __
 | |  | |_) | |>  <           | |  | | |_)|
 |_|  |_.__/|_/_//_/          |_|  |_| .__/
                                     | |    
-                                    |_| `)
+                                    |_| `);
 console.log(
-    'Thanks for installing rblx-rip! Keep in mind some asset types may need to be onsale.')
+  "Thanks for installing rblx-rip! Keep in mind some asset types may need to be onsale."
+);
 // i dont remember which types need it, lol
-main()
+main();
 async function main() {
   if (!fs.existsSync("./assets/")) {
     try {
-      fs.mkdirSync("./assets")
+      fs.mkdirSync("./assets");
     } catch (e) {
       console.error("Error!");
       throw e;
     }
   }
-  let asset = parseInt(await readline.questionAsync('Enter an asset ID:  '))
+  let asset = parseInt(await readline.questionAsync("Enter an asset ID:  "));
   if (isNaN(asset)) {
-    console.log("You can't input a string")
-    return main()
+    console.log("You can't input a string");
+    return main();
   }
-  let type = await readline.questionAsync('Enter the asset type:  ')
-  console.log(`Ok, ripping the asset id ${asset}`)
+  let type = await readline.questionAsync("Enter the asset type:  ");
+  console.log(`Ok, ripping the asset id ${asset}`);
   if (obj[type.toLowerCase()]) {
-    type = obj[type.toLowerCase()]
-    rip()
+    type = obj[type.toLowerCase()];
+    rip();
+  } else {
+    console.log("You entered an invalid type, please try again.");
+    return main();
   }
-  else {console.log('You entered an invalid type, please try again.')
-        return main()
-    }
-    async function rip() {
-    if (type == '.rbxm' || type == '.mp3' || type == '.webm' ||
-        type == ".rbxl") {
-      response = await download(
-          asset, {method : 'GET', headers : {'User-Agent' : 'Roblox/WinInet'}});
-      response.body.pipe(fs.createWriteStream(`./assets/${asset}${type}`))
-    } else if (type == '.rbxmx') {
-      resp = await download(asset).then(res => res.text())
-      newId = resp.split(";")
+  async function rip() {
+    if (
+      type == ".rbxm" ||
+      type == ".mp3" ||
+      type == ".webm" ||
+      type == ".rbxl"
+    ) {
+      response = await download(asset, {
+        method: "GET",
+        headers: { "User-Agent": "Roblox/WinInet" },
+      });
+      response.body.pipe(fs.createWriteStream(`./assets/${asset}${type}`));
+    } else if (type == ".rbxmx") {
+      resp = await download(asset).then((res) => res.text());
+      newId = resp.split(";");
       for (i = 0; newId.length > i; i++) {
-        res = await download(newId[i])
-        response.body.pipe(fs.createWriteStream(`./assets/${newId[i]}${type}`))
+        res = await download(newId[i]);
+        response.body.pipe(fs.createWriteStream(`./assets/${newId[i]}${type}`));
       }
-    } else if (type == '.k') {
-      console.log("This is coming soon, this isn't done yet.")
-    } else if (type == '.m') {
-      response = await download(asset).then(res => res.text())
+    } else if (type == ".k") {
+      console.log("This is coming soon, this isn't done yet.");
+    } else if (type == ".m") {
+      response = await download(asset).then((res) => res.text());
       result1 = response.split(
-          /<Content name="TextureId"><url>http:\/\/www.roblox.com\/asset\/\?id=([0-9]+)/)
+        /<Content name="TextureId"><url>http:\/\/www.roblox.com\/asset\/\?id=([0-9]+)/
+      );
       result2 =
-          /<Content name="MeshId"><url>http:\/\/www.roblox.com\/asset\/\?id=([0-9]+)/
-              .exec(response)
-      response2 = await download(result1[3])
-      response3 = await download(result2[1])
-      response2.body.pipe(fs.createWriteStream(`./assets/${result1[3]}.png`))
-      response3.body.pipe(fs.createWriteStream(`./assets/${result2[1]}.obj`))
-    } else if (type == '.z') {
-      response = await download(asset).then(res => res.text())
-      result1 = /MeshId.*?rbxassetid:\/\/([0-9]+)/.exec(response)
-      result2 = /TextureId.*?rbxassetid:\/\/([0-9]+)/.exec(response)
+        /<Content name="MeshId"><url>http:\/\/www.roblox.com\/asset\/\?id=([0-9]+)/.exec(
+          response
+        );
+      response2 = await download(result1[3]);
+      response3 = await download(result2[1]);
+      response2.body.pipe(fs.createWriteStream(`./assets/${result1[3]}.png`));
+      response3.body.pipe(fs.createWriteStream(`./assets/${result2[1]}.obj`));
+    } else if (type == ".z") {
+      response = await download(asset).then((res) => res.text());
+      result1 = /MeshId.*?rbxassetid:\/\/([0-9]+)/.exec(response);
+      result2 = /TextureId.*?rbxassetid:\/\/([0-9]+)/.exec(response);
       if (result2 == null) {
-        result2 = /http:\/\/www.roblox.com\/asset\/\?id=([0-9]+)/.exec(response)
+        result2 = /http:\/\/www.roblox.com\/asset\/\?id=([0-9]+)/.exec(
+          response
+        );
       }
-      response2 = await download(result1[1])
-      response3 = await download(result2[1])
-      response2.body.pipe(fs.createWriteStream(`./assets/${result1[1]}.obj`))
-      response3.body.pipe(fs.createWriteStream(`./assets/${result2[1]}.png`))
+      response2 = await download(result1[1]);
+      response3 = await download(result2[1]);
+      response2.body.pipe(fs.createWriteStream(`./assets/${result1[1]}.obj`));
+      response3.body.pipe(fs.createWriteStream(`./assets/${result2[1]}.png`));
     } else {
-      response = await download(asset).then(res => res.text())
-      newId = response.split("<url>")
-                  .join()
-                  .split("</url>")
-                  .join()
-                  .split(",")[1]
-                  .replace(/\D/g, '')
-      res = await download(newId)
-      res.body.pipe(fs.createWriteStream(`./assets/${newId}${type}`))
+      response = await download(asset).then((res) => res.text());
+      newId = response
+        .split("<url>")
+        .join()
+        .split("</url>")
+        .join()
+        .split(",")[1]
+        .replace(/\D/g, "");
+      res = await download(newId);
+      res.body.pipe(fs.createWriteStream(`./assets/${newId}${type}`));
     }
-    readline.close()
-    }
+    readline.close();
+  }
 }
 
 async function download(asset, headers) {
-        return await fetch(
-            `https://assetdelivery.roblox.com/v1/asset?id=${asset}`,
-            headers ? headers : {})
+  return await fetch(
+    `https://assetdelivery.roblox.com/v1/asset?id=${asset}`,
+    headers ? headers : {}
+  );
 }
