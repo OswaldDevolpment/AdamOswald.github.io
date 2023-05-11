5ab5c5a3b0b1903d7aae5ab302a516d2d3e55437
Restyled.io <commits@restyled.io>
2023-05-11 14:54:17 +0000

    Restyled by autopep8

--- rbxdl.py
+++ rbxdl.py
@@ -1,60 +1,66 @@
-import requests, argparse, time, os, random
+import requests
+import argparse
+import time
+import os
+import random
 from ast import literal_eval
-#Still have yet to find a way to reduce this... 
+# Still have yet to find a way to reduce this...
 astTypes = {
-  0:['Unknown', ''],
-  1:['Image', '.png'],
-  2:['TeeShirt', '.xml'],
-  3:['Audio', '.xml'],
-  4:['Mesh', ''],
-  5:['Lua', ''], 
-  8:['Hat', '.xml'],
-  9:['Place', '.rbxl'],
-  10:['Model', '.rbxm'],
-  11:['Shirt', '.xml'],
-  12:['Pants', '.xml'],
-  13:['Decal', '.xml'],
-  17:['Head', '.xml'],
-  18:['Face', '.xml'],
-  19:['Gear', '.xml'],
-  21:['Badge', ''],
-  24:['Animation', '.xml'],
-  27:['Torso', '.xml'],
-  28:['RightArm', '.xml'],
-  29:['LeftArm', '.xml'],
-  30:['LeftLeg', '.xml'],
-  31:['RightLeg', '.xml'],
-  32:['Package', '.xml'],
-  33:['YouTubeVideo', ''],
-  34:['GamePass', '.xml'],
-  38:['Plugin', '.xml'],
-  39:['SolidModel', '.xml'],
-  40:['MeshPart', '.rbxm'],
-  41:['HairAccessory', '.xml'],
-  42:['FaceAccessory', '.xml'],
-  43:['NeckAccessory', '.xml'],
-  44:['ShoulderAccessory', '.xml'],
-  45:['FrontAccessory', '.xml'],
-  46:['BackAccessory', '.xml'],
-  47:['WaistAccessory', '.xml'],
-  48:['ClimbAnimation', ''],
-  49:['DeathAnimation', ''],
-  50:['FallAnimation', ''],
-  51:['IdleAnimation', ''],
-  52:['JumpAnimation', ''],
-  53:['RunAnimation', ''],
-  54:['SwimAnimation', ''],
-  55:['WalkAnimation', ''],
-  56:['PoseAnimation', ''],
-  57:['EarAccessory', '.xml'],
-  58:['EyeAccessory', '.xml'],
-  61:['EmoteAnimation', ''],
-  62:['Video', '']   
+    0: ['Unknown', ''],
+    1: ['Image', '.png'],
+    2: ['TeeShirt', '.xml'],
+    3: ['Audio', '.xml'],
+    4: ['Mesh', ''],
+    5: ['Lua', ''],
+    8: ['Hat', '.xml'],
+    9: ['Place', '.rbxl'],
+    10: ['Model', '.rbxm'],
+    11: ['Shirt', '.xml'],
+    12: ['Pants', '.xml'],
+    13: ['Decal', '.xml'],
+    17: ['Head', '.xml'],
+    18: ['Face', '.xml'],
+    19: ['Gear', '.xml'],
+    21: ['Badge', ''],
+    24: ['Animation', '.xml'],
+    27: ['Torso', '.xml'],
+    28: ['RightArm', '.xml'],
+    29: ['LeftArm', '.xml'],
+    30: ['LeftLeg', '.xml'],
+    31: ['RightLeg', '.xml'],
+    32: ['Package', '.xml'],
+    33: ['YouTubeVideo', ''],
+    34: ['GamePass', '.xml'],
+    38: ['Plugin', '.xml'],
+    39: ['SolidModel', '.xml'],
+    40: ['MeshPart', '.rbxm'],
+    41: ['HairAccessory', '.xml'],
+    42: ['FaceAccessory', '.xml'],
+    43: ['NeckAccessory', '.xml'],
+    44: ['ShoulderAccessory', '.xml'],
+    45: ['FrontAccessory', '.xml'],
+    46: ['BackAccessory', '.xml'],
+    47: ['WaistAccessory', '.xml'],
+    48: ['ClimbAnimation', ''],
+    49: ['DeathAnimation', ''],
+    50: ['FallAnimation', ''],
+    51: ['IdleAnimation', ''],
+    52: ['JumpAnimation', ''],
+    53: ['RunAnimation', ''],
+    54: ['SwimAnimation', ''],
+    55: ['WalkAnimation', ''],
+    56: ['PoseAnimation', ''],
+    57: ['EarAccessory', '.xml'],
+    58: ['EyeAccessory', '.xml'],
+    61: ['EmoteAnimation', ''],
+    62: ['Video', '']
 }
-#urls used in program
+# urls used in program
 astUrl = 'https://assetdelivery.roblox.com/v1/asset?id='
 apiUrl = 'https://api.roblox.com/marketplace/productinfo?assetId='
-#Creates web requests and handles most errors and status codes
+# Creates web requests and handles most errors and status codes
+
+
 def makeWebReq(url):
     try:
         resp = requests.get(url)
@@ -63,29 +69,36 @@ def makeWebReq(url):
     except requests.RequestException as e:
         print("Exception occured whilst making request. This has been logged.")
         writeLogs(e)
-#used for getting metadata of an asset
-def getMeta(astId, specific = None):
+# used for getting metadata of an asset
+
+
+def getMeta(astId, specific=None):
     resp = makeWebReq(f'{apiUrl}{astId}')
     return resp[1].json().get(specific) or resp[1].json() if resp[0] == 200 else 0
-#Reduces amount of code I need to write
+# Reduces amount of code I need to write
+
+
 def createDirectory(dirName):
     if not os.path.isdir(str(dirName)):
         os.mkdir(str(dirName))
-    return(str(dirName)) 
-#Save asset to file
+    return (str(dirName))
+# Save asset to file
+
+
 def saveAsset(astId, astTypeStr, cDir, sDirName, astData, astVer):
     try:
         createDirectory(cDir)
         createDirectory(f'{cDir}\\{astTypeStr}')
-        saveLocation = createDirectory(f'{cDir}\\{astTypeStr}\\{astId}') if sDirName is True else f'{cDir}\\{astTypeStr}'
+        saveLocation = createDirectory(
+            f'{cDir}\\{astTypeStr}\\{astId}') if sDirName is True else f'{cDir}\\{astTypeStr}'
         fileName = f'{saveLocation}\\{astId}-version{astVer}' if astVer is not None else f'{saveLocation}\\{astId}'
-        with open(f'{fileName}{astTypes[getMeta(astId, "AssetTypeId")][1]}','wb+') as assetSave:
+        with open(f'{fileName}{astTypes[getMeta(astId, "AssetTypeId")][1]}', 'wb+') as assetSave:
             assetSave.write(astData)
         jsonMeta = getMeta(astId)
         if jsonMeta != 0:
             metaSaveLoc = f'{fileName}-META.txt'
             if not os.path.isfile(metaSaveLoc):
-                with open(f'{metaSaveLoc}', 'a', encoding = 'utf-8') as metaFile:
+                with open(f'{metaSaveLoc}', 'a', encoding='utf-8') as metaFile:
                     for i in jsonMeta:
                         if i == 'Creator':
                             metaFile.write('Creator: \n')
@@ -97,7 +110,9 @@ def saveAsset(astId, astTypeStr, cDir, sDirName, astData, astVer):
     except OSError as e:
         writeLogs(e)
         return e
-#Download asset  
+# Download asset
+
+
 def download(astId, astVer, args):
     cDir = args.dir if args.dir is not None else 'Downloaded'
     sDir = args.sdirs
@@ -106,7 +121,8 @@ def download(astId, astVer, args):
     resp = makeWebReq(url)
     if resp[0] == 200:
         print(f'Saving: {url}...')
-        save = saveAsset(astId, astTypes[getMeta(astId, 'AssetTypeId')][0], cDir, sDir, resp[1].content, astVer)
+        save = saveAsset(astId, astTypes[getMeta(
+            astId, 'AssetTypeId')][0], cDir, sDir, resp[1].content, astVer)
         if save == 1:
             print('Saved asset sucessfully!')
         else:
@@ -119,23 +135,31 @@ def download(astId, astVer, args):
     else:
         print(f'Could not download due to {resp[0]}')
     return 0
-#attempt to get every version of the asset
+# attempt to get every version of the asset
+
+
 def allVer(astId, args):
     aVer = 1
     while True:
         if download(astId, aVer, args) == 0:
             break
         aVer += 1
+
+
 def writeLogs(msg):
     with open('rbxdl.log', 'a') as logFile:
         logFile.write(f'{msg}\n\n')
-#Reduces code
+# Reduces code
+
+
 def startDL(astId, astVer, args, getAll=False):
     if getAll:
         allVer(astId, args)
     else:
-        return download(astId, astVer, args)        
-#Handle the user input 
+        return download(astId, astVer, args)
+# Handle the user input
+
+
 def handleArgs(args):
     astId = literal_eval(args.assetid)
     dlm = args.downlmode
@@ -148,17 +172,19 @@ def handleArgs(args):
             for i in astId:
                 startDL(i, astVer, args, getAll)
         else:
-            raise ValueError('Incorrect format for bulk downloading. Should be laid out as [id1,id2,id3,etc..]')
+            raise ValueError(
+                'Incorrect format for bulk downloading. Should be laid out as [id1,id2,id3,etc..]')
     elif dlm == 'range':
         if isinstance(astId, list) and len(astId) == 2:
             for i in range(astId[0], astId[1]+1):
                 startDL(i, astVer, args, getAll)
         else:
-            raise ValueError('Incorrect format for range downloading. Should be laid out as [minId, maxId]')
+            raise ValueError(
+                'Incorrect format for range downloading. Should be laid out as [minId, maxId]')
     elif dlm == 'roulette':
         rlAmn = args.rltAmnt if args.rltAmnt is not None else 1
         rlType = args.rltType
-        for _ in range(1,rlAmn+1):
+        for _ in range(1, rlAmn+1):
             while True:
                 canDl = True
                 randomId = random.randint(1000, 5000000000)
@@ -168,16 +194,24 @@ def handleArgs(args):
                 ):
                     canDl = False
                 if canDl and startDL(randomId, None, args) == 1:
-                    break                
-#argparse 
+                    break
+
+
+# argparse
 cmdParse = argparse.ArgumentParser(description='Download assets from ROBLOX.')
-cmdParse.add_argument('downlmode', choices=['single', 'bulk', 'range', 'roulette'], help='mode for asset downloading', type=str)
+cmdParse.add_argument('downlmode', choices=[
+                      'single', 'bulk', 'range', 'roulette'], help='mode for asset downloading', type=str)
 cmdParse.add_argument('assetid', help='id(s) of asset', type=str)
-cmdParse.add_argument('--dir', help='save assets into your own directory', type=str)
+cmdParse.add_argument(
+    '--dir', help='save assets into your own directory', type=str)
 cmdParse.add_argument('--ver', help='version(s) of the asset(s)', type=str)
-cmdParse.add_argument('--sdirs', help='save assets in their own directories', action='store_true')
-cmdParse.add_argument('--allVer', help='Download all of the versions of an asset (slow)', action='store_true')
-cmdParse.add_argument('--rltAmnt', help='How many times should the roulette download something?', type=int)
-cmdParse.add_argument('--rltType', help='What specific asset type should the roulette look for?', type=int)
+cmdParse.add_argument(
+    '--sdirs', help='save assets in their own directories', action='store_true')
+cmdParse.add_argument(
+    '--allVer', help='Download all of the versions of an asset (slow)', action='store_true')
+cmdParse.add_argument(
+    '--rltAmnt', help='How many times should the roulette download something?', type=int)
+cmdParse.add_argument(
+    '--rltType', help='What specific asset type should the roulette look for?', type=int)
 args = cmdParse.parse_args()
 handleArgs(args)
